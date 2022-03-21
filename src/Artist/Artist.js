8dce0b7e03c6958f2fceeeb63be10fadbbd4c4ed
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-03-21 11:21:16 +0100

    Pass toString_NominalCaseWithoutDateOfBirth_Success Test

--- src/Artist/Artist.js
+++ src/Artist/Artist.js
@@ -23,7 +23,9 @@ module.exports = class Artist{
      * @param dateOfBirth
      */
     constructor (firstname, lastname, dateOfBirth = null) {
-        throw new Error();
+        this.#firstname = firstname
+        this.#lastname = lastname
+        this.#dateOfBirth = dateOfBirth
     }
 
     /**
@@ -33,7 +35,10 @@ module.exports = class Artist{
      * @exception throws DateOfBirthException if DateOfBirth was not provided
      */
     toString(withDateOfBirth = false){
-        throw new Error();
+        if (withDateOfBirth){
+            return this.#firstname + ' ' + this.#lastname + '' + this.#dateOfBirth
+        }
+        return this.#firstname + ' ' + this.#lastname
     }
     //endregion public methods
 
