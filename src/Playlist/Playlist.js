99568747c97085b450797612acf3a96dcf47a97d
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-03-21 11:48:23 +0100

    Pass allGetters_NominalCase_Success Test

--- src/Playlist/Playlist.js
+++ src/Playlist/Playlist.js
@@ -22,11 +22,12 @@ module.exports = class Playlist {
      * @param songs : Song[] of songs
      */
     constructor(title, songs) {
-        throw new Error();
+        this.#title = title
+        this.#songs = songs
     }
 
     get title(){
-        throw new Error();
+        return this.#title
     }
 
     /**
@@ -34,7 +35,7 @@ module.exports = class Playlist {
      *        Sum of all songs length.
      */
     get length(){
-        throw new Error();
+        return this.songs.map(song => song.length).reduce((prev, next) => prev + next);
     }
 
     /**
@@ -42,7 +43,7 @@ module.exports = class Playlist {
      * @returns songs
      */
     get songs(){
-        throw new Error();
+        return this.#songs
     }
 
     /**
